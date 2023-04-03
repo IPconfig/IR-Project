3055c3e34a2e94abaa7bfe449ca0c204754e3991
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2023-04-03 17:35:48 +0200

    display individual document page

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ from models import Document
 
 app = create_app()
 
+doc = Document(
+    document_id=0,
+    title='The Great British Sorting Machine: Adolescents’ future in the balance of family, school and the neighborhood',
+    date='2018',
+    authors='Mijs, Jonathan J.B. (author), Nieuwenhuis, J.G. (author)',
+    abstract=' In this paper we incorporate both processes to shed a new light on a classic concern in the sociology of stratification: how are adolescents’ aspirations, expectations, and school performance shaped by the combined socioeconomic contexts of family, school and neighborhood life?',
+    doctype='working paper',
+    subjects='Adolescents; neighborhood effects; education; cultural resource perspective; reference group theory; United Kingdom; ALSPAC',
+    publisher='London School of Economics and Political Science',
+    series='Working Papers - LSE International Inequalities Institute (26)',
+    collection='Institutional Repository'
+)
+
+mock_documents = [doc, doc, doc]
 
 @app.route("/")
 def index():
-    doc = Document(
-        title='The Great British Sorting Machine: Adolescents’ future in the balance of family, school and the neighborhood',
-        date='2018',
-        authors='Mijs, Jonathan J.B. (author), Nieuwenhuis, J.G. (author)',
-        abstract='Research calls attention to the divergent school and labor market trajectories of&lt;br/&gt;Europe’s youth while, across the Atlantic, researchers describe the long-lasting&lt;br/&gt;consequences of poverty on adolescent development. In this paper we incorporate both processes to shed a new light on a classic concern in the sociology of stratification: how...',
-        doctype='working paper',
-        subjects=['Adolescents, neighborhood effects, education, cultural resource perspective, reference group theory, United Kingdom, ALSPAC'],
-        publisher='London School of Economics and Political Science',
-        series='Working Papers - LSE International Inequalities Institute (26)',
-        collection='Institutional Repository'
-    )
+    return render_template('search.html', documents=mock_documents, query='sorting machine')
 
-
-    return render_template('search.html', documents=[doc, doc, doc], query='sorting machine')
+@app.route('/document/<document_id>')
+def document(document_id):
+    return render_template('document.html', document=doc)
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 class Document:
-    def __init__(self, title, date, authors, subjects, abstract, doctype, publisher, series, collection):
+    def __init__(self, document_id, title, date, authors, subjects, abstract, doctype, publisher, series, collection):
+        self.document_id = document_id
         self.title = title
         self.date = date
         self.authors = authors
